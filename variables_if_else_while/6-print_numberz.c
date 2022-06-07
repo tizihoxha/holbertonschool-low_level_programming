c281d91c04fa774c31883aca0ea867d8ffb9f39b
tizihoxha <tizihoxha@gmail.com>
2022-06-07 07:11:21 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int c;
+
+for (c = '0'; c <= '9'; c++)
+{
+
+	putchar (c);
+}
+putchar ('\n');
+
+return (0);
+}
