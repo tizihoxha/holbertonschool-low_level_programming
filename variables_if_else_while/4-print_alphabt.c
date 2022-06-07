924a484172543421b63a2b50f3efdf53f987edd1
tizihoxha <tizihoxha@gmail.com>
2022-06-07 07:01:22 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char c;
+char e = 'e';
+char q = 'q';
+
+for (c = 'a'; c <= 'z'; c++)
+{
+	if (c != e && c != q)
+	putchar(c);
+}
+
+putchar('\n');
+return (0);
+}
