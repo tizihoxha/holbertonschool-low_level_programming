1d7574d33584f62f62a572d6258013dc1201daa3
tizihoxha <tizihoxha@gmail.com>
2022-06-09 06:20:10 -0700

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit -- entry point
+ * @n: intiger
+ * Return: value of last
+ */
+int print_last_digit(int n)
+
+{
+	int last = (n % 10);
+
+	if (last < 0)
+	{
+		last *= -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
