122ded68d9ab41971564e8291d3b1e3066cf06bd
tizihoxha <tizihoxha@gmail.com>
2022-06-09 05:27:51 -0700

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: int code will check for this character
+ *
+ * Return: the absolute value of the intinger
+ */
+
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
