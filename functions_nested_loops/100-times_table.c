ef6a77e9361fe1909f614e8be89905a368f55936
tizihoxha <tizihoxha@gmail.com>
2022-06-10 07:57:26 -0700

    task 12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ * @n: int code checks
+ * Return: successful 0
+ */
+
+void print_times_table(int n)
+{
+	int n1, n2, exp;
+
+	if (n >= 0 && n < 16)
+	{
+		for (n1 = 0; n1 < n; n1++)
+		{
+			for (n2 = 0; n2 < n; n2++)
+			{
+				exp = (n1 * n2);
+				if (n2 == 0)
+					_putchar(exp + '0');
+				else if (exp < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(exp + '0');
+				}
+				else if (exp >= 10 && exp < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((exp / 10) + '0');
+					_putchar((exp % 10) + '0');
+				}
+				else if (exp >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((exp / 100) + '0');
+					_putchar(((exp % 100) / 10) + '0');
+					_putchar((exp % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int print_last_digit(int);
+void print_times_table(int n);
