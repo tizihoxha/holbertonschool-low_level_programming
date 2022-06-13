2f7bb890074615561b15234c5fc89b31135d332a
tizihoxha <tizihoxha@gmail.com>
2022-06-13 04:36:59 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line- Entry point
+ *
+ * Return: n is 0 or less, the function should only print \n
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
