4fe4f66475aacd039bad6a6d9e70d03b228caa5c
tizihoxha <tizihoxha@gmail.com>
2022-06-13 03:16:18 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Return: numers from 0 to 9
+*/
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
+
