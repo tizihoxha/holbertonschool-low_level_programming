3318a351fe8f1258bdb7251a6ea7a5db8ced7c33
tizihoxha <tizihoxha@gmail.com>
2022-06-15 07:27:02 -0700

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Entry point
+ * @a: pointer
+ * @n: int, number of elements to be printed in an array
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
+
