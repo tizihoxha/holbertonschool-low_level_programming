018ae0c7c32fa4043bd8de5cc9cec0799eeecc90
tizihoxha <tizihoxha@gmail.com>
2022-06-16 05:34:24 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -26,8 +26,10 @@ char *cap_string(char *str)
 		str[i] == ')' ||
 		str[i] == '{' ||
 		str[i] == '}')
-		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
+		}
 	}
 	return (str);
 }
