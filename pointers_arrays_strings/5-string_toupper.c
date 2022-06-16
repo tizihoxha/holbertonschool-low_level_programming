f0cd45c1a9cc66e0cb2ebcc631d1659135a3bb5c
tizihoxha <tizihoxha@gmail.com>
2022-06-16 04:24:46 -0700

    added task

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - Entry point
+ * @str: string
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
