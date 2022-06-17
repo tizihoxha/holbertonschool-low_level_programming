f345d59b059282d6932d05d7d4d0d50fc17a3fdf
tizihoxha <tizihoxha@gmail.com>
2022-06-17 03:28:34 -0700

    task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ * @src: source
+ * @dest: destination
+ * @n: unsigned int
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
