ed6fab3c0b11367503c41cf6d75a5848f65856f8
tizihoxha <tizihoxha@gmail.com>
2022-06-17 07:05:00 -0700

    task 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: pointer to 2 d string
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
+
+
