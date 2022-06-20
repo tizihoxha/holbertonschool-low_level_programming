2035d2e190d2eef82940f394aa7873fba211f201
tizihoxha <tizihoxha@gmail.com>
2022-06-20 07:27:01 -0700

    task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+/**
+ * find_prime_number - Entry point
+ * @n: int
+ * @i: int
+ * Return: 1 , 0
+ */
+int find_prime_number(int n, int i)
+{
+	if (n % i == 0)
+	{
+		if (n == i)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + find_prime_number(n, i + 1));
+}
+/**
+ * find_prime_number - Entry point
+ * @n: int
+ * Return: 1, 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime_number(n, 2));
+}
