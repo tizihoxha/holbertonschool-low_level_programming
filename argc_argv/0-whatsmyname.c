c678331d6545d58bd268eb17d34343761a57ba69
tizihoxha <tizihoxha@gmail.com>
2022-06-22 02:44:02 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array that contains prog arg
+ * Return: 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
