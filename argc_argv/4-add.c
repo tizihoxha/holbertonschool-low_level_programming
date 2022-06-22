8cece78c8cc974c772123b47214826c0f8bd8303
tizihoxha <tizihoxha@gmail.com>
2022-06-22 04:02:25 -0700

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array that contains prog arg
+ * Return: 0, 1
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
