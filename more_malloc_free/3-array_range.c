0d5ed9ace3c5ccbe107b33b2172bf04010b4c156
tizihoxha <tizihoxha@gmail.com>
2022-06-27 06:35:53 -0700

    task 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * array_range( - Entry point
+ * @min: int
+ * @max: int
+ * Return: array
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+	int i;
+	int length;
+
+	length = (max - min) + 1;
+	if (min > max)
+		return (NULL);
+	array = malloc(sizeof(*array) * length);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < length; i++)
+	{
+		array[i] = min++;
+	}
+	return (array);
+}
