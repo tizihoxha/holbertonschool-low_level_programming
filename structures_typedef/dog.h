9b413ba4a457a6e5a719c903ff532582c219ce30
tizihoxha <tizihoxha@gmail.com>
2022-06-29 02:06:00 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,7 +3,7 @@
 /**
  * dog_p - typedef for struct dog
  */
-typedef struct dog dog_p;
+typedef struct dog dog_t;
 
 /**
  * struct dog - stores several infos for dog
@@ -20,7 +20,7 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_p *new_dog(char *name, float age, char *owner);
-void free_dog(dog_p *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
