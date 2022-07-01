7527df9b631f5e7ab37ec107584b1a62d051dc2f
tizihoxha <tizihoxha@gmail.com>
2022-07-01 03:16:49 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function
+ * @size: size of array
+ * @array: array
+ * @action: pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+			for (i = 0; i < size; i++)
+			action(array[i]);
+}
