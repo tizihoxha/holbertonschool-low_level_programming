da8c8f2a38881aaf0c4010ed48a38acaf9dc48be
tizihoxha <tizihoxha@gmail.com>
2022-07-01 04:40:54 -0700

    task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -45,9 +45,13 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
-
 /**
  * op_mod - modulo of 2 nr
  * @a: nr 1
@@ -57,5 +61,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
