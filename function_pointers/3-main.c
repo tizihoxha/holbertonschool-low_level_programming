6865b06c2a7ac8ec8386c4cf18cdc3f56562ee04
tizihoxha <tizihoxha@gmail.com>
2022-07-01 04:35:37 -0700

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*funcptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	funcptr = get_op_func(argv[2]);
+
+	if (funcptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", funcptr(a, b));
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ * @a: nr 1
+ * @b: nr 2
+ *
+ * Return: add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subctracts
+ * @a: nr 1
+ * @b: nr 2
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies
+ * @a: nr 1
+ * @b: nr 2
+ *
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides
+ * @a: nr 1
+ * @b: nr 2
+ *
+ * Return: division.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of 2 nr
+ * @a: nr 1
+ * @b: nr 2
+ *
+ * Return: remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
