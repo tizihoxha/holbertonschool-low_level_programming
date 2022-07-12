9a408b10a87601e6a64388147b213aa993a9de67
tizihoxha <tizihoxha@gmail.com>
2022-07-12 09:50:50 -0700

    task 5

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -2,11 +2,11 @@
 
 void before_main(void)__attribute((constructor));
 /**
- * first_before_main(void) - function to print text before main funcion is executed
+ * first_before_main - function to print text before main funcion is executed
  * Return: no return
  */
 void first_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
