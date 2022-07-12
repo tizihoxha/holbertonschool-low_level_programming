855df1f0cd2b4a75de9cae751b01d7d407b214f0
tizihoxha <tizihoxha@gmail.com>
2022-07-12 09:52:52 -0700

    task 5

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void before_main(void)__attribute((constructor));
+void first_before_main(void)__attribute((constructor));
 /**
  * first_before_main - function to print text before main funcion is executed
  * Return: no return
