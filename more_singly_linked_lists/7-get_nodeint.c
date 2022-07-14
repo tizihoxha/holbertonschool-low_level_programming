476fe7a24ec5cb1044ce2875d9f60a1052053723
tizihoxha <tizihoxha@gmail.com>
2022-07-14 04:29:15 -0700

    task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - function that deletes the head node and returns the
- * head node data n
+ * get_nodeint_at_index - function that returns the nth node of a listint_t linked list
  * @head: head of the node
- * Return: i
+ * @index: unsigned int
+ * Return: head
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
