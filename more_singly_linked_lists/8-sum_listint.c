64c6954e9ec22bb478729eb3fd6e6b60d02cfec7
tizihoxha <tizihoxha@gmail.com>
2022-07-14 04:47:33 -0700

    task 8

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_listint - function that returns the sum of all 
+ * sum_listint - function that returns the sum of all
  * the data (n) of a listint_t linked list
  * @head: head of the node
  * Return: sum
