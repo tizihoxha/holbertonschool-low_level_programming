1f95150ab59e0a14d1192dc80fa3a657cb03fdd9
tizihoxha <tizihoxha@gmail.com>
2022-07-14 01:50:19 -0700

    task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_listint - function that prints all the elements of a node
  * @h: head of the node
@@ -8,7 +7,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes_number = 0;
-
+	
 	while (h)
 	{
 		printf("%d\n", h->n);
