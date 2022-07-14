45f93668f76f275f1686486c765a65781967e3ab
tizihoxha <tizihoxha@gmail.com>
2022-07-14 02:23:28 -0700

    task 2

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * add_nodeint - function that adds a new node to the linked list
+ * @head: head of the single linked list
+ * Return: new_node also set as head node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (head == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
