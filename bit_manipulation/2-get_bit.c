4febb282c76c6ab213c45dfb7c8102b12f97e71f
tizihoxha <tizihoxha@gmail.com>
2022-07-18 08:32:09 -0700

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 /**
  * get_bit - function that returns the value of a bit at a given index
  * @n: unsigned long int
- * @index: unsigned int 
+ * @index: unsigned int
  * Return: value of the bit at index or -1 on error
  */
 int get_bit(unsigned long int n, unsigned int index)
