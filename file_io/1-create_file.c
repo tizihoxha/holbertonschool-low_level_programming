46464edeb315f780876a378f84da306234d83e66
tizihoxha <tizihoxha@gmail.com>
2022-07-19 06:40:23 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[i])
 			i++;
 	}
-	o = open(filename, O_CREAT, O_RDWR, O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		w = write(o, text_content, i);
 		if (o == -1)
 		{
