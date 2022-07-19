859ddd6b90f6ae0eb6722e1d48b98daca93c66fc
tizihoxha <tizihoxha@gmail.com>
2022-07-19 07:36:40 -0700

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,21 +19,23 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[i])
 			i++;
 	}
+
 	o = open(filename, O_WRONLY | O_APPEND);
-		w = write(o, text_content, i);
-		if (o == -1)
-		{
-			return (-1);
-		}
-		if (w == -1)
-		{
-			return (-1);
-		}
-		if (w != i)
-		{
-			return (-1);
-		}
-		close(o);
-		return (1);
+	w = write(o, text_content, i);
+	
+	if (o == -1)
+	{
+		return (-1);
+	}
+	if (w == -1)
+	{
+		return (-1);
+	}
+	if (w != i)
+	{
+		return (-1);
+	}
+	close(o);
+	return (1);
 }
 
