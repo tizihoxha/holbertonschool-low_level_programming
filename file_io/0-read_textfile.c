a67841d3704310481d418d09057900b71625dc64
tizihoxha <tizihoxha@gmail.com>
2022-07-19 05:39:44 -0700

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,32 +10,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t o, r, w;
 	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
+		return (0);
+
+	 buffer = malloc(sizeof(char) * letters);
+        if (buffer == NULL)
 		return (0);
 	o = open(filename, O_RDONLY);
-	if (o == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
-
 	r = read(o, buffer, letters);
-	if (r == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	buffer[r] = '\0';
-
 	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1)
+	if ((o == -1) || (r == -1) || (w == -1) || (w != r))
 	{
 		free(buffer);
 		return (0);
 	}
+
 	free(buffer);
-	return (w);
 	close(o);
+	return (w);
 }
 
