b3a9e908e013da901ed6010986c481ee5dccdc25
tizihoxha <tizihoxha@gmail.com>
2022-07-19 07:48:48 -0700

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w;
-	int i;
+	int i, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,6 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		i = 0;
 		while (text_content[i])
 			i++;
+		len++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
