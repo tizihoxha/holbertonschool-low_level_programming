f9da1b04b86b5f8d2bfd3c00d83ca70de6229d62
tizihoxha <tizihoxha@gmail.com>
2022-07-20 05:46:31 -0700

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@
 int main(int argc, char **argv)
 {
 	int file_from, file_to, cls;
-	ssize_t w, length =  1024;
+	ssize_t w, r = 1024;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -19,16 +19,16 @@ int main(int argc, char **argv)
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (length == 1024)
+	while (r == 1024)
 	{
-		length = read(file_from, buffer, 1024);
-		if (length < 0)
+		r = read(file_from, buffer, 1024);
+		if (r < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		w = write(file_to, buffer, length);
-		if (file_from < 0 || w != length)
+		w = write(file_to, buffer, 1024);
+		if (file_from < 0 || w != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
