f15cd6463a547bdda0d9df70ce896ce6a7a808de
tizihoxha <tizihoxha@gmail.com>
2022-07-20 04:18:59 -0700

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * main - function that copies the contents of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int file_from, file_to, cls;
+	ssize_t w, length =  1024;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to \n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (length == 1024)
+	{
+		length = read(file_from, buffer, 1024);
+		if (length < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		w = write(file_to, buffer, length);
+		if (file_from < 0 || w != length)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	cls = close(file_from);
+	if (cls < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	cls = close(file_to);
+	if (cls < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
+
+
--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
