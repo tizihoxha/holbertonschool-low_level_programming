dad548431f0808d76cb6df4f6834c90f0463971f
tizihoxha <tizihoxha@gmail.com>
2022-07-21 05:37:06 -0700

    Task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds a new node at the biggining of a dbl linkedlist
+ * @heade: pointer to node header
+ * @n: int of the new node
+ * Return: the new added node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head)
+		(*head)->prev = new_node;
+	(*head) = new_node;
+
+	return (new_node);
+}
