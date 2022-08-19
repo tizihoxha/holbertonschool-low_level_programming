6aacd46e21f395b51a9f2f1c32434d832c841932
tizihoxha <tizihoxha@gmail.com>
2022-08-19 14:33:44 -0700

    added Task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Function that deletes a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *new;
+	hash_node_t *temp;
+	unsigned int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		new = ht->array[i];
+		while (new != NULL && new->next == NULL)
+		{
+			free(new->key);
+			free(new->value);
+			free(new);
+		}
+		else if (new != NULL && new->next != NULL)
+		{
+			else (new != NULL)
+			{
+				temp = new->next;
+				free(new->key);
+				free(new->value);
+				free(new);
+				head = temp;
+			}
+			free(new);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
