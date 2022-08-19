63bb13741d72c27e4259a9e541719d68da2cbe2b
tizihoxha <tizihoxha@gmail.com>
2022-08-19 03:32:53 -0700

    fixed betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,8 +4,8 @@
  * hash_table_set - function to add a element to the hash table
  * @key: key
  * @ht: hash table
- * @value: value 
- * Return 0 if failure, 1 in success
+ * @value: value
+ * Return: 0 if failure, 1 in success
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (temp != NULL)
 	{
-		if(strcmp(temp->key, key) == 0)
+		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
 			temp->value = strdup(value);
