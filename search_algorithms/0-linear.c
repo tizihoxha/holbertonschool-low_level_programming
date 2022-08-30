3b44bd21009b6c933f182e80ef6e6a563823c5fd
tizihoxha <tizihoxha@gmail.com>
2022-08-30 11:48:27 -0700

    added task 0, searchalgos.h

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,11 +13,9 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	i = 0;
-	while (i < size)
+	for (i  = 0; i < size; i++)
 	{
 		printf("VALUE checked array[%ld] = [%d]/n", i, array[i]);
-		i++;
 		if (array[i] == value)
 			return (i);
 	}
