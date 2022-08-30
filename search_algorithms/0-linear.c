b19c6cbf45013a5f1d7468555354deb73c60e91d
tizihoxha <tizihoxha@gmail.com>
2022-08-30 11:56:47 -0700

    fixes

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,13 +11,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
-	for (i  = 0; i < size; i++)
+	i = 0;
+	while (i < size)
 	{
 		printf("VALUE checked array[%ld] = [%d]/n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+		i++;
 	}
 	return (-1);
 }
