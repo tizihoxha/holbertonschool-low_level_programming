23c8286c9fb883f444e57a88b03e8222f46a4dc2
tizihoxha <tizihoxha@gmail.com>
2022-08-30 13:21:00 -0700

    betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- *
- *
- *
- *
- *
+ * linear_search - function that uses linear search algorithm
+ * @array: pointer to the array of elements
+ * @size: number of elements or size of the array
+ * @value: value of the element we're searching
+ * Return: -1 failure, element success
  */
 int linear_search(int *array, size_t size, int value)
 {
