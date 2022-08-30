62e1103917a4d5f6791352ce0d4900b4a4a73301
tizihoxha <tizihoxha@gmail.com>
2022-08-30 11:42:13 -0700

    added task 0, searchalgos.h & readme

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		printf("VALUE checked array[%ld] = [%d]/n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
